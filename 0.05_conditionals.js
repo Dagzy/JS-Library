fab269a5fcfdc1948ae81a969652c48f4b8c5058
Dagzy <quincye.williams@gmail.com>
2017-06-07 16:00:38 -0400

    Added functions

--- 0.05_conditionals.js
+++ 0.05_conditionals.js
@@ -13,5 +13,29 @@ if (colts > patriots){
 
 var num = 1
 console.log(num);
+//This is a FOR loop.
+for (i=1; i <= 100; i++) {
+	if(i % 3 == 0 && i % 5 == 0){
+		console.log("FizzBuzz");
+	}else if (i % 3 == 0){
+		console.log("Fizz");
+	}else if (i % 5 == 0){
+		console.log("Buzz");
+	}else{
+		console.log(i);
+	}
+}
 
-for (i=1; i <= 100); i++;
+// Use WHILE here
+var num = 1;
+while(num<101){
+	if(num % 3 == 0 && num % 5 == 0){
+	console.log("FizzBuzz");
+}else if(num % 3 == 0){
+	console.log("Fizz");
+}else if(num % 5 == 0){
+	console.log("Buzz");
+}else{
+	console.log(num);
+}num + = 1;
+}
